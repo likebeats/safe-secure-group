cd38b40826c665a5b88932b7826d08b1f6ba6f74
Manpreet <likebeats@gmail.com>
2015-02-08 22:56:35 -0500

    payment mode changes

--- models/Payment.js
+++ models/Payment.js
@@ -5,7 +5,7 @@ var keystone = require('keystone'),
 
 var Payment = new keystone.List('Payment', {
     map: { name: 'paymentDate' },
-    defaultSort: '-paymentDate'
+    defaultSort: '-paymentDate -_id'
 });
 
 var monthNames = [ { value: 0, label: "January" },
@@ -25,24 +25,79 @@ Payment.add({
     paymentDate: { type: Types.Date, format: 'MMM DD, YYYY', default: Date.now, initial: false, required: true },
     parkingSpot: { type: Types.Relationship, ref: 'ParkingSpot', required: true, initial: true /*, filters: { customer: ':customer' }*/ },
     customer: { type: Types.Relationship, ref: 'Customer', required: false, initial: false },
-    paymentForMonth: { type: Types.Select, numeric: true, default: moment().format("M")-1, options: monthNames, emptyOption: true, required: false, initial: true },
-    paymentForYear: { type: Types.Number, format: false, default: new Date().getFullYear(), required: true, initial: true },
+    paymentFrom: { type: Types.Date, format: 'MMM DD, YYYY', required: true, initial: true, hidden: true },
+    paymentTo: { type: Types.Date, format: 'MMM DD, YYYY', required: false, initial: true, hidden: true },
+    paymentForMonth: { type: Types.Select, numeric: true, options: monthNames, emptyOption: true, initial: false },
+    paymentForYear: { type: Types.Number, format: false, initial: false },
     paymentAmount: { type: Types.Money, required: true, initial: true },
     paymentMethod: { type: Types.Select, options: 'check, cash', default: 'check', emptyOption: false },
     checkNumber: { type: String, dependsOn: { paymentMethod: 'check' } },
     lateFeeAmount: { type: Types.Money }
 });
 
+Payment.schema.pre('validate', function(next) {
+
+    if (!this.paymentFrom) this.paymentFrom = new Date();
+    next();
+
+});
 
 Payment.schema.pre('save', function(next) {
 
     var payment = this;
-    ParkingSpot.model.findOne().where('_id').equals(this.parkingSpot).exec(function(err, spot) {
-        payment.customer = spot.customer;
-        next();
-    });
 
-})
+    payment.paymentDate = new Date(payment.paymentDate.setHours(0,0,0,0));
+
+    if (!payment.customer) {
+
+        ParkingSpot.model.findOne().where('_id').equals(this.parkingSpot).exec(function(err, spot) {
+            var customer = spot.customer;
+            payment.customer = customer;
+
+            if (payment.paymentFrom) {
+                payment.paymentForMonth = payment.paymentFrom.getMonth();
+                payment.paymentForYear = payment.paymentFrom.getFullYear();
+            }
+
+            if (payment.paymentTo) {
+
+                payment.paymentFrom.setDate(1);
+                payment.paymentTo.setDate(1);
+
+                var current = payment.paymentFrom;
+                var end = payment.paymentTo;
+
+                if (current.getTime() !== end.getTime()) {
+                    current = new Date(current.setMonth(current.getMonth() + 1));
+                    while(current <= end) {
+                        console.log(current);
+
+                        // create multiple payment objects
+                        var newPayment = new Payment.model();
+                        newPayment.paymentDate = payment.paymentDate;
+                        newPayment.parkingSpot = payment.parkingSpot;
+                        newPayment.customer = payment.customer;
+                        newPayment.paymentAmount = payment.paymentAmount;
+                        newPayment.paymentForMonth = current.getMonth();
+                        newPayment.paymentForYear = current.getFullYear();
+                        newPayment.paymentFrom = new Date(current);
+                        newPayment.save();
+
+                        var newDate = current.setMonth(current.getMonth() + 1);
+                        current = new Date(newDate);
+                    }
+                }
+
+            }
+
+            next();
+        });
+
+    } else {
+        next();
+    }
+
+});
 
 Payment.defaultColumns = 'paymentDate, parkingSpot, customer, paymentForMonth, paymentForYear, paymentAmount';
 Payment.register();
--- routes/views/index.js
+++ routes/views/index.js
@@ -7,6 +7,44 @@ var keystone = require('keystone'),
 
 exports = module.exports = function(req, res) {
 
+    // Payment.model.find(function(err, payments) {
+
+    //     console.log(payments.length);
+    //     if (err) {
+    //         console.log("find error:" + err);
+    //         return;
+    //     }
+
+    //     async.eachSeries(payments, function(payment, next) {
+
+    //         var d = moment(payment.paymentDate).zone('+0300');
+    //         d = d.date(d.date()+1);
+    //         d = d.hour(0).minute(0).second(0);
+    //         payment.paymentDate = new Date(d);
+
+    //         console.log(d + " ||| " + payment.paymentDate);
+
+    //         payment.save(function(err) {
+
+    //             if (!err) {
+    //                 console.log("payment saved");
+    //             }
+    //             else {
+    //                 console.log("Error: could not save payment: " + err);
+    //             }
+
+    //             next();
+
+    //         });
+
+    //     }, function(err) {
+
+    //         console.log("async error:" + err);
+
+    //     });
+
+    // });
+
     var view = new keystone.View(req, res),
         locals = res.locals;
 
@@ -46,9 +84,6 @@ exports = module.exports = function(req, res) {
                 }
 
                 async.each(parkingSpots, function(parkingSpot, next2) {
-                //for (var ps = 0; ps < parkingSpots.length; ps++) {
-
-                    //var parkingSpot = parkingSpots[ps];
 
                     Payment.model.find().where('parkingSpot').equals(parkingSpot._id)
                                         .where('customer').equals(parkingSpot.customer)
@@ -111,117 +146,6 @@ exports = module.exports = function(req, res) {
         });
 
     }
-
-
-    // parkingNumber, KeyCode1, company, fullName, street, city/state, zipCode, phone, email
-
-//     view.on('init', function(next) {
-
-//         locals.csvContent = [];
-//         csv.fromPath("records.csv")
-//         .on("data", function(data){
-
-//             locals.csvContent.push(data);
-
-//         })
-//         .on("end", function(){
-
-
-//             async.each(locals.csvContent, function(data, next) {
-//             //for (var i = 0; i < locals.csvContent.length; i++) {
-
-//                 //var data = locals.csvContent[i];
-
-//                 var parkingNumber = data[0];
-//                 var keyCode1 = data[1];
-//                 var company = data[2];
-//                 var fullName = data[3];
-//                 var street = data[4];
-//                 var cityAndState = data[5];
-//                 var zipCode = data[6];
-//                 var phone = data[7];
-//                 var email = data[8];
-
-//                 var nameParts = fullName.split(" ");
-//                 var firstName = null;
-//                 var lastName = null;
-//                 if (nameParts.length == 1) {
-//                     firstName = nameParts[0];
-//                     lastName = '';
-//                 } else if (nameParts.length == 2) {
-//                     firstName = nameParts[0];
-//                     lastName = nameParts[1];
-//                 } else if (nameParts.length == 3) {
-//                     firstName = nameParts[0] + " " + nameParts[1];
-//                     lastName = nameParts[2];
-//                 }
-
-//                 //create Customer
-//                 if (firstName && lastName) {
-//                     //console.log(nameParts + "      " + firstName + "/" + lastName);
-
-//                     var cityParts = cityAndState.split(", ");
-//                     var city = cityParts[0] ? cityParts[0] : null;
-//                     var state = cityParts[1] ? cityParts[1] : null;
-
-//                     var address = null;
-//                     if (street) {
-//                         address = {
-//                             street1: street,
-//                             suburb: city,
-//                             state: state,
-//                             postCode: zipCode,
-//                             country: 'United States'
-//                         }
-//                     }
-
-
-//                     console.log("FINDING CUSTOMER");
-//                     var customerModel = null;
-//                     Customer.model.findOne().where('customerName.first').equals(firstName)
-//                                         .where('customerName.last').equals(lastName)
-//                                         .where('company').equals(company)
-//                                         .exec(function(err, customer)
-//                     {
-//                         console.log("FINDING FINISHED");
-//                         if (!customer) {
-
-
-//                         } else {
-//                             //console.log("CUSTOMER ALREADY EXISTS!");
-//                             next(err);
-//                         }
-//                     });
-
-//                 }
-
-
-// //                                             var newCustomer = new Customer.model({
-// //                                 customerName: {
-// //                                     first: firstName,
-// //                                     last: lastName
-// //                                 },
-// //                                 company: company,
-// //                                 address: address,
-// //                                 email: email,
-// //                                 phone: phone,
-// //                                 keyNumberOne: keyCode1
-// //                             });
-// //                             //console.log("saving: " + newCustomer.customerName);
-
-// //                             newCustomer.save(function(err) {
-// //                                 next(err);
-// //                             });
-
-//             }, function(err) {
-//                 next(err);
-// 			});
-
-//         });
-
-//     });
-
-
 
     // Render the view
     view.render('index');
